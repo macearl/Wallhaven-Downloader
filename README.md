63d422ca8cb039ff5ec1c67a99eb958878997b8a
MacEarl <oomacearloo@googlemail.com>
2015-10-08 23:12:44 +0200

    added command line options, added help text, moved changelog to separate file

--- README.md
+++ README.md
@@ -6,87 +6,12 @@
 </p>
 
 <p>
-	<h3>This Script is written for GNU Linux, it should work under Mac OS</h3>
-	<br />
+	You can configure it directly inside the script or per command line arguments (command line arguments will override settings made directly in the script)
+	<br/>
+	check `./wallhaven -h` for details
 </p>
 
 <p>
-	<strong>Changelog :</strong>
-	<ul>
-		<li>
-			<strong>Revision 0.1.6.4</strong><br />
-			<ol>
-				<li>added a starting page number (by ry167)</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.6.3</strong><br />
-			<ol>
-				<li>added -m 1 option to grep command to prevent downloading every wallpaper twice</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.6.2</strong><br />
-			<ol>
-				<li>sorting variable now affects search results, thanks to munhyunsu for pointing it out</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.6.1</strong><br />
-			<ol>
-				<li>added http prefix to referer</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.6</strong><br />
-			<ol>
-				<li>fixed issue with login token</li>
-				<li>added useragent to wget to fix "403 forbidden" error</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.5</strong><br />
-			<ol>
-				<li>fixed issue if all wallpapers on a page where already downloaded</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.4</strong><br />
-			<ol>
-				<li>fixed parallel mode</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.3</strong><br />
-			<ol>
-				<li>added check if downloaded.txt file exists</li>
-				<li>added "--gnu" option to parallel<br />
-				(for some older Distributions which set the default mode to tollef) <br />
-				For some older Versions of parallel remove the "--no-notice" option if you get an error like this: <br />
-				"parallel: Error: Command (--no-notice) starts with '-'. Is this a wrong option?"</li>
-				<li>fixed issue where wget would not automatically add a "http://" prefix</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.2</strong><br />
-			<ol>
-				<li>fixed urls to work with latest wallhaven update</li>
-				<li>added some comments</li>
-				<li>fixed login issue when downloading favorites</li>
-				<li>merged normal and parallel version</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1.1</strong><br />
-			<ol>
-				<li>updated and tested parts of the script to work with newest wallhaven site (not all features tested)</li>
-			</ol>
-		</li>
-		<li>
-			<strong>Revision 0.1</strong><br />
-			<ol>
-				<li>first Version of script, most features from the wallbase script are implemented</li>
-			</ol>
-		</li>
-	</ul>
+	<h3>This Script is written for GNU Linux, it should work under Mac OS</h3>
+	<br />
 </p>
--- wallhaven.sh
+++ wallhaven.sh
@@ -4,54 +4,10 @@
 # This script is brought to you by MacEarl and is based on the
 # script for wallbase.cc (https://github.com/sevensins/Wallbase-Downloader)
 #
-#
 # This Script is written for GNU Linux, it should work under Mac OS
-#
-#
-# Revision 0.1.6.4
-# 1. added a starting page number (by ry167)
-#
-# Revision 0.1.6.3
-# 1. added -m 1 option to grep command to prevent downloading every wallpaper twice
-# 
-# Revision 0.1.6.2
-# 1. sorting variable now affects search results, thanks to munhyunsu for pointing it out
-#
-# Revision 0.1.6.1
-# 1. added http prefix to referer
-#
-# Revision 0.1.6
-# 1. fixed issue with login token
-# 2. added useragent to wget to fix "403 forbidden" error
-#
-# Revision 0.1.5
-# 1. fixed issue if all wallpapers on a page where already downloaded
-#
-# Revision 0.1.4
-# 1. fixed parallel mode
-#
-# Revision 0.1.3
-# 1. added check if downloaded.txt file exists
-# 2. added "--gnu" option to parallel 
-# (for some older Distributions which set the default mode to tollef)
-# For some older Versions of parallel remove the "--no-notice" option if you get an error like this:
-# "parallel: Error: Command (--no-notice) starts with '-'. Is this a wrong option?"
-# 3. fixed issue where wget would not automatically add a "http://" prefix 
-#
-# Revision 0.1.2
-# 1. fixed urls to work with latest wallhaven update
-# 2. added some comments
-# 3. fixed login issue when downloading favorites
-# 4. merged normal and parallel version
-# 
-# Revision 0.1.1
-# 1. updated and tested parts of the script to work with 
-#    newest wallhaven site (not all features tested)
-#
-# Revision 0.1
-# 1. first Version of script, most features from the wallbase 
-#    script are implemented
-#
+
+REVISION=0.1.6.5
+
 #####################################
 ###   Needed for NSFW/Favorites   ###
 #####################################
@@ -63,50 +19,38 @@ PASS=""
 ### End needed for NSFW/Favorites ###
 #####################################
 
-####################################
-###     Configuration Options    ###
-####################################
+#####################################
+###     Configuration Options     ###
+#####################################
 # Where should the Wallpapers be stored?
 LOCATION=/location/to/your/wallpaper/folder
 # How many Wallpapers should be downloaded, should be multiples of 24 (right now they only use a fixed number of thumbs per page)
 WPNUMBER=48
 # What page to start downloading at, default and minimum of 1.
 STARTPAGE=1
-# Type standard (newest, oldest, random, hits, mostfav), search, favorites (for now only the default collection), useruploads (if selected, only purity variable will change the outcome)
+# Type standard (newest, oldest, random, hits, mostfav), search, favorites (for now only the default collection), useruploads (if selected, only FILTER variable will change the outcome)
 TYPE=standard
 # From which Categories should Wallpapers be downloaded, first number is for General, second for Anime, third for People, 1 to enable category, 0 to disable it
 CATEGORIES=111
-# Which Purity Wallpapers should be downloaded, first number is for sfw content, second for sketchy content, third for nsfw, 1 to enable, 0 to disable
-PURITY=110
+# Which Wallpapers should be filtered (FILTER) downloaded, first number is for sfw content, second for sketchy content, third for nsfw, 1 to enable, 0 to disable
+FILTER=110
 # Which Resolution should be downloaded, leave empty for all (most common resolutions possible, for details see wallhaven site)
 RESOLUTION=
-# Which aspectratio should be downloaded, leave empty for all (possible values: 4x3, 5x4, 16x9, 16x10, 32x9, 48x9)
-RATIO=
+# Which aspectASPECTRATIO should be downloaded, leave empty for all (possible values: 4x3, 5x4, 16x9, 16x10, 32x9, 48x9)
+ASPECTRATIO=
 # Which Type should be displayed (relevance, random, date_added, views, favorites)
-SORTING=random
+MODE=random
 # How should the Wallpapers be ordered (desc, asc)
 ORDER=desc
 # Searchterm, only used if TYPE = search
 QUERY="nature"
 # User from which Wallpapers should be downloaded (only used for TYPE=useruploads)
-USR=AksumkA
+USER=AksumkA
 # use gnu parallel to speed up the download (0, 1), if set to 1 make sure you have gnuparallel installed, see normal.vs.parallel.txt for speed improvements
 PARALLEL=0
-####################################
-###   End Configuration Options  ###
-####################################
-
-# creates Location folder if it does not exist
-if [ ! -d $LOCATION ]; then
-    mkdir -p $LOCATION
-fi
-
-cd $LOCATION
-
-# creates downloaded.txt if it does not exist
-if [ ! -f ./downloaded.txt ]; then
-    touch downloaded.txt
-fi
+#####################################
+###   End Configuration Options   ###
+#####################################
 
 #
 # logs in to the wallhaven website to give the user more functionality
@@ -182,8 +126,112 @@ function downloadWallpapers {
         fi
 } #/downloadWallpapers
 
+#
+# displays help text (valid command line arguments)
+#
+function helpText {
+    printf 'Usage: ./wallhaven.sh [OPTIONS]\n'
+    printf 'Download wallpapers from wallhaven.cc\n\n'
+    printf 'If no options are specified, default values from within the script will be used\n\n'
+    printf '%s -l, --location\t\t location where the wallpapers will be stored\n'
+    printf '%s -n, --number\t\t Number of Wallpapers to download\n'
+    printf '%s -s, --startpage\t page to start downloading from\n'
+    printf '%s -t, --type\t\t Type of download Operation: standard, search, \n\t\t\t favorites, useruploads\n'
+    printf '%s -c, --categories\t categories to download from, eg. 111 for General,\n\t\t\t Anime and People, 1 to include, 0 to exclude\n'
+    printf '%s -f, --filter\t\t filter out content based on purity rating, eg. 111 \n\t\t\t for SFW, sketchy and NSFW content, 1 to include, \n\t\t\t 0 to exclude\n'
+    printf '%s -r, --resolution\t resolutions to download, separate mutliple \n\t\t\t resolutions by ,\n'
+    printf '%s -a, --aspectratio\t only download wallpaper with given aspectratios, \n\t\t\t separate multiple aspectratios by ,\n'
+    printf '%s -m, --mode\t\t sorting mode for wallpapers: relevance, random,\n\t\t\t date_added, views, favorites \n'
+    printf '%s -o, --order\t\t order ascending (asc) oder descending (desc)\n'
+    printf '%s -q, --query\t\t search query, eg. '\''mario'\'', single quotes needed,\n\t\t\t for searching exact phrases use double quotes \n\t\t\t inside single quotes, eg. '\''"super mario"'\'' \n'
+    printf '%s -u, --user\t\t download wallpapers from given user\n'
+    printf '%s -p, --parallel\t\t make use of gnu parallel (1 to enable, 0 to disable)\n'
+    printf '%s -v, --version\t\t show current version\n'
+    printf '%s -h, --help\t\t show this help text and exit\n\n'
+    printf 'Examples:\n'
+    printf './wallhaven.sh -l ~/wp/ -n 48 -s 1 -t standard -c 101 -f 111 -r 1920x1080 \n\t       -a 16x9 -m random -o desc -p 1\n\n'
+    printf 'Download 48 random wallpapers with a resolution of 1920x1080 and \nan aspectratio of 16x9 to ~/wp/ starting with page 1 from the \ncategories general and people including SFW, sketchy and NSWF Content\nwhile utilizing gnu parallel\n\n'
+    printf './wallhaven.sh -l ~/wp/ -n 48 -s 1 -t search -c 111 -f 111 -r 1920x1080 \n\t       -a 16x9 -m relevance -o desc -q '\''"super mario"'\'' -p 1\n\n'
+    printf 'Download 48 wallpapers related to the search query "super mario" with\na resolution of 1920x1080 and an aspectratio of 16x9 to ~/wp/ starting\nwith page 1 from the categories general, anime and people including SFW,\nsketchy and NSWF Content while utilizing gnu parallel\n\n\n'
+    printf 'latest version available at: <https://github.com/macearl/Wallhaven-Downloader>'
+} # helptext
+
+# Command line Arguments
+while [[ $# -ge 1 ]]
+    do
+    key="$1"
+
+    case $key in
+        -l|--location)
+            LOCATION="$2"
+            shift;;
+        -n|--number)
+            WPNUMBER="$2"
+            shift;;
+        -s|--startpage)
+            STARTPAGE="$2"
+            shift;;
+        -t|--type)
+            TYPE="$2"
+            shift;;
+        -c|--categories)
+            CATEGORIES="$2"
+            shift;;
+        -f|--filter)
+            FILTER="$2"
+            shift;;
+        -r|--resolution)
+            RESOLUTION="$2"
+            shift;;
+        -a|--aspectratio)
+            ASPECTRATIO="$2"
+            shift;;
+        -m|--mode)
+            MODE="$2"
+            shift;;
+        -o|--order)
+            ORDER="$2"
+            shift;;
+        -q|--query)
+            QUERY=$(echo "$2" | sed s/\'//g)
+            shift;;
+        -u|--user)
+            USER="$2"
+            shift;;
+        -p|--parallel)
+            PARALLEL="$2"
+            shift;;
+        -h|--help)
+            helpText
+            exit
+            ;;
+        -v|--version)
+            printf "Wallhaven Downloader $REVISION"
+            exit
+            ;;
+        *)
+            printf "unknown option: $1\n"
+            helpText
+            exit
+            ;;
+    esac
+    shift # past argument or value
+    done
+
+# creates Location folder if it does not exist
+if [ ! -d $LOCATION ]; then
+    mkdir -p $LOCATION
+fi
+
+cd $LOCATION
+
+# creates downloaded.txt if it does not exist
+if [ ! -f ./downloaded.txt ]; then
+    touch downloaded.txt
+fi
+
 # login only when it is required ( for example to download favourites or nsfw content... )
-if [ $PURITY == 001 ] || [ $PURITY == 011 ] || [ $PURITY == 111 ] || [ $TYPE == favorites ] ; then
+if [ $FILTER == 001 ] || [ $FILTER == 011 ] || [ $FILTER == 111 ] || [ $TYPE == favorites ] ; then
    login $USER $PASS
 fi
 
@@ -191,7 +239,7 @@ if [ $TYPE == standard ]; then
     for (( count=0, page="$STARTPAGE"; count< "$WPNUMBER"; count=count+24, page=page+1 ));
     do
         printf "Download Page $page"
-        getPage "search?page=$page&categories=$CATEGORIES&purity=$PURITY&resolutions=$RESOLUTION&ratios=$RATIO&sorting=$SORTING&order=$ORDER"
+        getPage "search?page=$page&categories=$CATEGORIES&purity=$FILTER&resolutions=$RESOLUTION&ratios=$ASPECTRATIO&sorting=$MODE&order=$ORDER"
         printf "\n    - done!\n"
         printf "Download Wallpapers from Page $page"
         downloadWallpapers
@@ -203,7 +251,7 @@ elif [ $TYPE == search ] ; then
     for (( count=0, page="$STARTPAGE"; count< "$WPNUMBER"; count=count+24, page=page+1 ));
     do
         printf "Download Page $page"
-        getPage "search?page=$page&categories=$CATEGORIES&purity=$PURITY&resolutions=$RESOLUTION&ratios=$RATIO&sorting=$SORTING&order=desc&q=$QUERY"
+        getPage "search?page=$page&categories=$CATEGORIES&purity=$FILTER&resolutions=$RESOLUTION&ratios=$ASPECTRATIO&sorting=$MODE&order=desc&q=$QUERY"
         printf "\n    - done!\n"
         printf "Download Wallpapers from Page $page"
         downloadWallpapers
@@ -229,7 +277,7 @@ elif [ $TYPE == useruploads ] ; then
     for (( count=0, page="$STARTPAGE"; count< "$WPNUMBER"; count=count+24, page=page+1 ));
     do
         printf "Download Page $page"
-        getPage "user/$USR/uploads?page=$page&purity=$PURITY"
+        getPage "user/$USER/uploads?page=$page&purity=$FILTER"
         printf "\n    - done!\n"
         printf "Download Wallpapers from Page $page"
         downloadWallpapers
